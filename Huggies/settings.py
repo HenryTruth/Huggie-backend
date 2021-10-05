56058b4712f18f4871d7fe4bcb7e99ddaf01700c
Truth <henrysempire111gmail.com>
2021-10-05 10:21:17 -0700

    fixed a bug in the settings.py

--- Huggies/settings.py
+++ Huggies/settings.py
@@ -17,6 +17,7 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 import django_heroku
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,5 +157,7 @@ cloudinary.config(
   api_secret = "k9tjvzXstvMkFIuqlJFm4_t_tcA",
 #   secure = True
 )
+
+SOCKET_SERVER = config("SOCKET_SERVER")
 
 django_heroku.settings(locals())
