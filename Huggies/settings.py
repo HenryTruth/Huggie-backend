0443b5f61457dcba19da629abca9dbf66d630119
Truth <henrysempire111gmail.com>
2021-10-05 11:26:06 -0700

    fixed the remaining conflict

--- Huggies/settings.py
+++ Huggies/settings.py
@@ -15,13 +15,8 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-<<<<<<< HEAD
 from decouple import config
 import django_heroku
-=======
-import django_heroku
-from decouple import config
->>>>>>> master
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,13 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-!#)@_9cklm-eo&-=np&q=ct$xu5_v8^ep1l_g+unlw5#r)3-3e'
-<<<<<<< HEAD
 SECRET_KEY = config("SECRET_KEY")
 # 5432
-=======
-SECRET_KEY = config('SECRET_KEY')
-
->>>>>>> master
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
