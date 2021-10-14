5449dd31a83d1fda605ae7010bec5832dd69d9f4
Truth <henrysempire111gmail.com>
2021-10-14 15:39:23 -0700

    added a cors allow origin3

--- Huggies/settings.py
+++ Huggies/settings.py
@@ -163,14 +163,6 @@ CORS_ALLOW_HEADERS = (
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:8080",
-    "https://localhost:8081",
-    "http://127.0.0.1:8081",
-    "https://127.0.0.1:8081",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
