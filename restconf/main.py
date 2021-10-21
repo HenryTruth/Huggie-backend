5f71a9aee7eaa44beb51fdce01db30244f7f6c02
Truth <henrysempire111gmail.com>
2021-10-21 14:25:49 -0700

    updated the expiration correctly

--- restconf/main.py
+++ restconf/main.py
@@ -50,6 +50,7 @@ JWT_AUTH = {
     # 'JWT_ISSUER': None,
 
     'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=14),
     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
 
     'JWT_AUTH_HEADER_PREFIX': 'JWT',
